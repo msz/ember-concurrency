68329015c8d69ea9b5b411a67c10e70af8c910df
machty <machty@gmail.com>
2017-07-08 13:37:11 -0400

    fix cancel-all messaging, allow duck-typing

--- addon/-helpers.js
+++ addon/-helpers.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
-import { Task } from 'ember-concurrency/-task-property';
 
-export function taskHelperClosure(taskMethod, _args, hash) {
+export function taskHelperClosure(helperName, taskMethod, _args, hash) {
   let task = _args[0];
   let outerArgs = _args.slice(1);
 
   return Ember.run.bind(null, function(...innerArgs) {
-    if (!(task instanceof Task)) {
-      Ember.assert(`The first argument passed to the \`perform\` helper should be a Task object (without quotes); you passed ${task}`, false);
+    if (!task || typeof task[taskMethod] !== 'function') {
+      Ember.assert(`The first argument passed to the \`${helperName}\` helper should be a Task object (without quotes); you passed ${task}`, false);
       return;
     }
 
